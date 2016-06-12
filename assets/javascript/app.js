d343177d7c8749f7e4fe1e05f80cd5852484d4f2
Ckk6 <tresk26@yahoo.com>
2016-06-11 23:58:25 -0400

    clickevents/html/array

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+
+//Create array to hold input box values
+//set initial array value to initial button
+
+ var  character = [prince]
+ var characterindex = 0
+
+
+
+
+ 				
+
+
+ //create an on click event for button use this notation.
+
+  $('button').on('click', function(){}
+    
+
+
+
+
+//create submit click event that:
+
+	//build a url using api parameters
+	//make ajax request
+	//console.log json
+	//move the images to the image area--prepend
+	//create click events for the new images so that they stop and start
+
+  
