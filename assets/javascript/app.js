90d7741a5375f6b4bb35256cd480120bc2ebfd7a
Ckk6 <tresk26@yahoo.com>
2016-06-13 16:29:17 -0400

    will_not_duplicate_buttons_or_create_blank_buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,78 @@
+$(document).ready(function() {
 
-//Create array to hold input box values
-//set initial array value to initial button
+	//Create array to hold input box values
+	//set initial array value to initial button
 
- var  character = [prince]
- var characterindex = 0
+ 	var  character = ['Prince'];
+ 	var charindex = 1;
+ 	var same = false
+ 	console.log(character);
 
+ 	function match(){
+ 		console.log('i am in match');
+ 		same = false;
+ 		for (charindex = 0; charindex < character.length; charindex++) {
+ 			 if ($("#new-music").val() == character[charindex]){
+ 			 	same = true;
+ 			 	$("#new-music").val("");
 
+ 			 }
+ 			 console.log(character[charindex]);
+ 			 console.log("same is " + same); 
+ 		}
+ 	}
 
+	//create submit click event that will:
+	$('#submit').on('click', function() {
+		console.log('i am in the on click function');
+		//capture userinput and push it to the array
+		if ($("#new-music").val() != "") {
+			console.log($("#new-music").val());
 
+			match();
+			console.log('i am back in click after match same is ' + same);
+
+			if (same == false){
+
+				var music = $("#new-music").val();
+				console.log("music is " + music);
+		
+			
+				character.push(music);
+
+				$('#button-area').empty();
+			
+
+				//loop through character array to create buttons
+
+	
+				for(charindex = 0; charindex < character.length; charindex++) {
+
+			  		mbutton = $('<button>').html(character[charindex]);
+					mbutton.attr('class', 'button');
+					mbutton.attr('type', 'button');
+					mbutton.attr('class', 'btn btn-primary button');
+					mbutton.attr('display', 'inline-block');
+				
+				
+					$('#button-area').append(mbutton);
+					$('#new-music').val("");
+		
+				}
+			}	
+		}	
+	});
+	
  				
 
 
  //create an on click event for button use this notation.
 
-  $('button').on('click', function(){}
+ // $('.button').on('click', function(){l, /`	.
+  	 
+       // var musician = $(this).data('musicican');
+       // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+ // }
     
 
 
@@ -27,4 +86,4 @@
 	//move the images to the image area--prepend
 	//create click events for the new images so that they stop and start
 
-  
+}); //end (document).ready 
