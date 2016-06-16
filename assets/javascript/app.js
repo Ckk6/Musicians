a20113327ac799c2000235c44a6f4efd6b8c7120
Ckk6 <tresk26@yahoo.com>
2016-06-16 13:14:31 -0400

    giphys

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
  	var charindex = 1;
  	var same = false
  	console.log(character);
+ 	var result;
 
  	function match(){
  		console.log('i am in match');
@@ -29,6 +30,7 @@ $(document).ready(function() {
 		if ($("#new-music").val() != "") {
 			console.log($("#new-music").val());
 
+			//ensure input is not already in the array
 			match();
 			console.log('i am back in click after match same is ' + same);
 
@@ -53,36 +55,88 @@ $(document).ready(function() {
 					mbutton.attr('type', 'button');
 					mbutton.attr('class', 'btn btn-primary button');
 					mbutton.attr('display', 'inline-block');
-				
+					mbutton.attr('data-person', character[charindex]);
 				
 					$('#button-area').append(mbutton);
+					//empty input area
 					$('#new-music').val("");
-		
+					//create click events for new buttons
+					$('.button').on('click', function() {
+						var artist = $(this).data('person');
+						console.log('artist is ' + artist);
+						console.log('$(this).data() ' + $(this).data('person'));
+						console.log(this);
+
+						
+						//create a query  public key "dc6zaTOxFJmzC”
+
+							//build a url using api parameters
+							var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + artist + "&api_key=dc6zaTOxFJmzC&limit=15";
+							//q - search query term or phrase
+							//limit - (optional) number of results to return, maximum 100. Default 25
+							//rating - limit results to those rated (y,g, pg, pg-13 or r).
+							//fmt - (optional) return results in html or json format (useful for viewing responses as GIFs to debug/test)
+						//do an ajax request
+						//make ajax request
+						$.ajax({
+							url: queryURL, 
+							method: 'GET'
+						})
+						.done(function(response)
+							{
+							//console.log json response	
+							
+							result = response.data;
+							  
+						
+							console.log(result); 
+
+						
+							//present 10 images with g or pg rating(include that in query)
+							for ( var imageIndex=0; imageIndex <10; imageIndex++) {
+								console.log (result[imageIndex].rating);
+								if (result[imageIndex].rating =="g" || result[imageIndex].rating =="pg") {
+									musicalArtist=$('<div>');
+									p = $('<p>');
+									p.text(result[imageIndex].rating); 
+
+
+									musicImage = $('<img>');
+									musicImage.attr('src', result[imageIndex].images.fixed_height.url);
+
+									musicalArtist.append(p);
+                    				musicalArtist.append(musicImage);
+                    				//$('#gifsAppearHere').prepend(gifDiv); 
+                    				$('#button-area').prepend(musicImage);
+
+								}// end if
+
+							}//end for
+
+						});//end of done
+
+						//each image to have click event so they stop and start 
+					});// end button click
+
 				}
-			}	
-		}	
-	});
+			}//end if same	
+		}//end empty input check	
+	});//end submit onclick function
 	
  				
 
 
- //create an on click event for button use this notation.
+ 
 
- // $('.button').on('click', function(){l, /`	.
-  	 
-       // var musician = $(this).data('musicican');
-       // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
- // }
-    
-
-
+ 
 
 
 //create submit click event that:
 
-	//build a url using api parameters
-	//make ajax request
-	//console.log json
+	
+	
+
+	
 	//move the images to the image area--prepend
 	//create click events for the new images so that they stop and start
 
